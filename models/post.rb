cc5a639ef55a932932a9306d28ecff83146b05a4
Yoshiya Hayashi <yoshiya.hayashi@livesense.co.jp>
2020-01-06 17:56:07 +0900

    @@tableを@tableに変更

--- models/post.rb
+++ models/post.rb
@@ -4,53 +4,51 @@ require 'csv'
 
 class Post
   def initialize
-    @@table = CSV.table('foo.csv')
+    @table = CSV.table('foo.csv')
   end
 
   def self.all
     Post.new
-    @@table
+    @table
   end
 
   def self.find(id)
     Post.new
-    @@table.select{|row| row if row.field?(id.to_i) }.at(0).to_h
+    @table.select{|row| row if row.field?(id.to_i) }.at(0).to_h
   end
 
   def self.create(title, content)
     Post.new
     check_id
-    @@table[@next_id] = [@next_id, title, content]
+    @table[@next_id] = [@next_id, title, content]
     save_table
   end
 
   def self.patch(id, title, content)
     Post.new
-    @@table[id.to_i] = [id, title, content]
+    @table[id.to_i] = [id, title, content]
     save_table
   end
 
   def self.delete(id)
     Post.new
-    @@table.delete(id.to_i)
-    #@@table[id] = ['', '', '']
+    @table.delete(id.to_i)
     save_table
   end
 
   def self.check_id
-    # binding.pry
-    if @@table[:id].last.nil?
+    if @table[:id].last.nil?
       @next_id = 0
     else
-      @next_id = @@table[:id].last + 1
+      @next_id = @table[:id].last + 1
     end
   end
 
   def self.save_table
     header = ['id', 'title', 'content']
-    CSV.open('foo.csv', 'w', headers: true) do |csv| # output to csv file
-      csv << header if @@table.headers.compact.empty?
-      @@table.to_a.each do |record|
+    CSV.open('foo.csv', 'w', headers: true) do |csv|
+      csv << header if @table.headers.compact.empty?
+      @table.to_a.each do |record|
         if !record.compact.empty?
           csv << record
         end
