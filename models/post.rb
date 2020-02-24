bd6162d73c5387d60b48ad727726de43b243d463
yoshuxaaaa <yoshiya.hayashi@livesense.co.jp>
2020-02-25 00:39:35 +0900

    allをクラスメソッドに変更

--- models/post.rb
+++ models/post.rb
@@ -9,8 +9,9 @@ class Post
     @connection = PG.connect(host: ENV['PG_HOST'], user: ENV['PG_USER'], password: ENV['PG_PASSWORD'], dbname: ENV['PG_DB'], port: ENV['PG_PORT'])
   end
 
-  def all
-    result = @connection.exec('SELECT * FROM books')
+  def self.all
+    Dotenv.load
+    result = PG.connect(host: ENV['PG_HOST'], user: ENV['PG_USER'], password: ENV['PG_PASSWORD'], dbname: ENV['PG_DB'], port: ENV['PG_PORT']).exec('SELECT * FROM books')
     array = []
     result.each do |tuple|
       array << Hash[:id, tuple['id'], :title, tuple['title'], :content, tuple['content']]
